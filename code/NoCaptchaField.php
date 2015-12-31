8f72d4b61920625f43192586d0e34497a7f7efa1
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:47:41 +1300

    Converted to PSR-2

--- code/NoCaptchaField.php
+++ code/NoCaptchaField.php
@@ -1,43 +1,42 @@
 <?php
 
-class NoCaptchaField extends FormField {
-	
-	
-	function Field($properties = array()) {
-		
-		
-		$api_key = $this->config()->PublicKey;
-		
-		return <<<HTML
+class NoCaptchaField extends FormField
+{
+    
+    
+    public function Field($properties = array())
+    {
+        $api_key = $this->config()->PublicKey;
+        
+        return <<<HTML
 <div class="g-recaptcha" data-sitekey="$api_key"></div>
 HTML;
-	}
-	
-	public function validate($validator) {
-		
-		if(
-			!isset($_REQUEST['g-recaptcha-response']) 
-			|| empty($_REQUEST['g-recaptcha-response'])
-		) {
-			$validator->validationError(
-				$this->name, 
-				_t(
-					'RecaptchaField.EMPTY', 
-					"Please check the 'I am not a robot' box"
-				), 
-				"validation", 
-				false
-			);
-			
-			return false;
-		}
-		
-		return true;
-		
-	}
-	
-	public function setFieldMapping($array) {
-		$this->spamFieldMapping = $array;
-	}
-	
+    }
+    
+    public function validate($validator)
+    {
+        if (
+            !isset($_REQUEST['g-recaptcha-response'])
+            || empty($_REQUEST['g-recaptcha-response'])
+        ) {
+            $validator->validationError(
+                $this->name,
+                _t(
+                    'RecaptchaField.EMPTY',
+                    "Please check the 'I am not a robot' box"
+                ),
+                "validation",
+                false
+            );
+            
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public function setFieldMapping($array)
+    {
+        $this->spamFieldMapping = $array;
+    }
 }
